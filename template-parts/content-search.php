3d78a9e25eaf1e9bc2eeba74934e06e4a5c19f15
Eduardo Zulian <eduardozulian@gmail.com>
2015-09-23 13:58:58 -0300

    Add post thumbnails for archives and search

--- template-parts/content-search.php
+++ template-parts/content-search.php
@@ -10,14 +10,24 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	<header class="entry-header flag">
+		<div class="flag__image">
+			<?php if ( has_post_thumbnail() ) : ?>
+			<div class="entry-image">
+				<a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_post_thumbnail( 'thumbnail' ); ?></a>
+			</div><!-- .entry-image -->
+			<?php endif; ?>
+		</div>
 
-		<?php if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php publico_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<div class="flag__body">
+			<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+
+			<?php if ( 'post' === get_post_type() ) : ?>
+			<div class="entry-meta">
+				<?php publico_posted_on(); ?>
+			</div><!-- .entry-meta -->
+			<?php endif; ?>
+		</div>
 	</header><!-- .entry-header -->
 
 	<div class="entry-summary">
