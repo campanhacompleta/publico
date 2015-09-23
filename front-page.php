c9cb911773f938f38dfd2754e57ecec148382d67
Eduardo Zulian <eduardozulian@gmail.com>
2015-09-23 15:11:38 -0300

    Remove hardcoded secondary structure
    Now, we have a widget area!

--- front-page.php
+++ front-page.php
@@ -96,65 +96,6 @@ get_header(); ?>
 
 			<?php get_sidebar( 'social-engagement' ); ?>
 
-			<div class="row">
-				<section class="site-extras site__section clearfix">
-					<div class="large-7 columns">
-						<div class="site-extras--video">
-							<h4 class="area__title">Vídeo</h4>
-							<?php
-							$video = new WP_Query( array (
-								'posts_per_page' => 1,
-								'ignore_sticky_posts' => true,
-								'tax_query' => array(
-									array(
-										'taxonomy' => 'post_format',
-										'field'    => 'slug',
-										'terms'    => array( 'post-format-video' ),
-									),
-								)
-							) );
-
-							if ( $video->have_posts() ) :
-								while ( $video->have_posts() ) : $video->the_post(); ?>
-
-								<article id="post-<?php the_ID(); ?>" <?php post_class( 'hentry--columns clear' ); ?>>
-									<header class="entry-header">
-
-										<?php publico_the_first_embed( $post->ID ); ?>
-
-										<div class="entry-video">
-											<?php echo $fist_embedded; ?>
-										</div><!-- .entry-video -->
-
-									</header><!-- .entry-header -->
-
-									<div class="entry-content">
-										<?php //the_content(); ?>
-									</div>
-								</article>
-
-							<?php
-								endwhile;
-								wp_reset_postdata();
-							endif;
-							?>
-						</div>
-					</div>
-					
-					<div class="large-5 columns">
-						<div class="site-extras--events">
-							<h4 class="area__title">Agenda</h4>
-							<ul class="no-bullet">
-								<li><a href="#" title="">II Seminário Internacional</a><ul><li>22/09/2015 - 24/09/2015</li><li>Recife</li></ul></li>
-								<li><a href="#" title="">9° Congresso Brasileiro</a><ul><li>28/09/2015 - 01/10/2015</li><li>Belém</li></ul></li>
-								<li><a href="#" title="">29° Encontro Nacional de Políticas Públicas </a><ul><li>28/09/2015 - 01/10/2015</li><li>Rio de Janeiro</li></ul></li>
-								<li class="all-events-link"><a href="#" class="button wide" title="todos os eventos">Ver todos os eventos</a></li>
-							</ul>
-						</div>
-					</div>
-				</section>
-			</div>
-
 			<?php get_sidebar( 'content-secondary' ); ?>
 
 		</main><!-- #main -->
