ed0434d5a1251ece237c932c4c7d04a3fa76150e
Eduardo Zulian <eduardozulian@gmail.com>
2015-09-29 11:37:27 -0300

    Add posted on function to our screen reader content

--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -12,6 +12,9 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
 		<?php the_title( '<h1 class="entry-title show-for-sr">', '</h1>' ); ?>
+		<div class="entry-meta show-for-sr">
+			<?php echo publico_get_posted_on(); ?>
+		</div><!-- .entry-meta -->
 		<?php if ( has_post_thumbnail() ) : ?>
 			<div class="entry-image">
 				<?php the_post_thumbnail( 'large' ); ?>
