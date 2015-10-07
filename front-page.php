cd2d6c2538f8d70a22c6882421a89e2a9926e3fc
Eduardo Zulian <eduardozulian@gmail.com>
2015-10-07 11:50:21 -0300

    Update flag object

--- front-page.php
+++ front-page.php
@@ -84,7 +84,7 @@ get_header(); ?>
 										<?php endif; ?>
 									</div>
 
-									<div class="flag__body">
+									<div class="flag__body flag__body--padded">
 										<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
 									</div>
 								</article><!-- #post-## -->
