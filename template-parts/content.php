056a1377a3d091fd53c86759c0d7c611cc0aaa75
Eduardo Zulian <eduardozulian@gmail.com>
2015-10-08 12:33:19 -0300

    Update post titles for archive, blog and search
    Fix #42

--- template-parts/content.php
+++ template-parts/content.php
@@ -31,20 +31,7 @@
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'publico' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-		?>
-
-		<?php
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'publico' ),
-				'after'  => '</div>',
-			) );
-		?>
+		<?php the_excerpt(); ?>
 	</div><!-- .entry-content -->
 
 	<footer class="entry-footer">
