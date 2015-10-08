b4f9b7cd8ccd72ccdb31847b0d13d7a45eab2047
Eduardo Zulian <eduardozulian@gmail.com>
2015-10-08 18:04:28 -0300

    Introducing grunt-wp-i18n

--- src/Gruntfile.js
+++ src/Gruntfile.js
@@ -91,13 +91,34 @@ module.exports = function(grunt) {
         spawn: false
       }
     },
+
+    makepot: {
+      target: {
+          options: {
+              cwd: '../',                          // Directory of files to internationalize.
+              domainPath: '/languages',                   // Where to save the POT file.
+              mainFile: 'style.css',                     // Main project file.
+              //potComments: '',                  // The copyright at the beginning of the POT file.
+              potFilename: 'publico.pot',                  // Name of the POT file.
+              potHeaders: {
+                  poedit: true,                 // Includes common Poedit headers.
+                  'x-poedit-keywordslist': true // Include a list of all possible gettext functions.
+              },                                // Headers to add to the generated POT file.
+              type: 'wp-theme',                // Type of project (wp-plugin or wp-theme).
+              updateTimestamp: true,             // Whether the POT-Creation-Date should be updated without other changes.
+              updatePoFiles: false              // Whether to update PO files in the same directory as the POT file.
+          }
+      }
+    }
   });
-    
+
   // Load npm tasks
   grunt.loadNpmTasks('grunt-sass');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-wp-i18n');
 
   // Register tasks
   grunt.registerTask('default', ['sass', 'watch']);
+  grunt.registerTask('i18n', 'makepot');
 }
